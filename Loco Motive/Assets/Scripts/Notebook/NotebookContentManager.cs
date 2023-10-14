0ba5193959e64f20b3215d249bbc6426ccfe826d
Karkaranos <karkaranosyt@gmail.com>
2023-10-14 17:43:57 -0500

    Updated Notebook
    Notebook has timeline and is easier to adjust content

--- Loco Motive/Assets/Scripts/Notebook/NotebookContentManager.cs
+++ Loco Motive/Assets/Scripts/Notebook/NotebookContentManager.cs
@@ -12,60 +12,68 @@ using UnityEngine;
 public class NotebookContentManager : MonoBehaviour
 {
     #region Variables
-    public int pageCount;
-    public string[,] notebookContent;
+    public int pageCount = 7;
+
+    public int timelineCount = 5;
+
     public Sprite[] image;
     public bool[,] contentVisible;
+    public bool[] timelineVisible;
     public Sprite empty;
     [SerializeField] private Sprite item1;
     [SerializeField] private Sprite item2;
+
+    public string[] page1notebookContent = new string[6];
+    public string[] page2notebookContent = new string[6];
+    public string[] page3notebookContent = new string[6];
+    public string[] page4notebookContent = new string[6];
+    public string[] page5notebookContent = new string[6];
+    public string[] page6notebookContent = new string[6];
+    public string[] page7notebookContent = new string[6];
+    public string[] page8notebookContent = new string[6];
+
+    public string[] timelinenotebookContent = new string[5];
+
+    public List<string[]> pages = new List<string[]>();
 
     public int ITEMS_PER_PAGE = 7;
     #endregion
 
     #region Functions
-    
+
     /// <summary>
     /// Start initializes the arrays and assigns data to each location
     /// </summary>
     void Start()
     {
-        notebookContent = new string[pageCount, ITEMS_PER_PAGE-1];
+        print(pageCount);
         image = new Sprite[pageCount];
-        contentVisible = new bool[pageCount,ITEMS_PER_PAGE];
+        timelineVisible = new bool[timelineCount];
+        contentVisible = new bool[pageCount, ITEMS_PER_PAGE];
 
-        for(int i=0; i<ITEMS_PER_PAGE; i++)
+        for (int i = 0; i < ITEMS_PER_PAGE; i++)
         {
-            for(int j=0; j<pageCount; j++)
+            for (int j = 0; j < pageCount; j++)
             {
                 contentVisible[j, i] = false;
             }
         }
 
-        //Page 1 Content
-        notebookContent[0, 0] = "Suspect 1";
-        notebookContent[0, 1] = "A photo of Alexander";
-        notebookContent[0, 2] = "Alexander, the Chief Financial Officer and Secons-In-Charge of Defiant Door Studios. Seems friendly enough";
-        notebookContent[0, 3] = "Very passionate about his son. His son, Joseph, was born September 9, 2003(9/9/03) and makes his father very happy.";
-        notebookContent[0, 4] = "Climbing the Corporate Ladder";
-        notebookContent[0, 5] = "Potential Motive";
+        for (int i = 0; i < timelineCount; i++)
+        {
+            timelineVisible[i] = false;
+        }
 
+        pages.Add(page1notebookContent);
+        pages.Add(page2notebookContent);
+        pages.Add(page3notebookContent);
+        pages.Add(page4notebookContent);
+        pages.Add(page5notebookContent);
+        pages.Add(page6notebookContent);
+        pages.Add(page7notebookContent);
+        pages.Add(timelinenotebookContent);
+        //pages.Add(page8notebookContent);
 
-        //Page 2 content
-        notebookContent[1, 0] = "Evidence 1";
-        notebookContent[1, 1] = "A Signed Photo of Corporate Ladder";
-        notebookContent[1, 2] = "This item was found in Suspect 1's room behind a locked box. It must be a highly prized posession.";
-        notebookContent[1, 3] = "Hunter was tempted to steal this item and keep it for himself. I tried to stop him from stealing it- We are detectives, not thieves. We would be acting in opposition to the law we try to uphold. I stopped him but he may have gone back and stolen it.\nThis item is dated April 20th (4/20)";
-        notebookContent[1, 4] = "Potential Suspect 1";
-        notebookContent[1, 5] = "Suspects for Item";
-
-        //Page 3 content
-        notebookContent[2, 0] = "Evidence 2";
-        notebookContent[2, 1] = "A scary-looking Plant";
-        notebookContent[2, 2] = "This item was found in Suspect 1's room behind a locked box.";
-        notebookContent[2, 3] = "More sample text describing the object, its location. Maybe this reveals after interrogating the right suspect. To differentiate this from the previous sample, here is more text.";
-        notebookContent[2, 4] = "Potential Suspect\nAnother Potential Suspect\nAnother Potential Suspect";
-        notebookContent[2, 5] = "Suspects for Item";
 
         //Assigning visuals
         image[0] = empty;
@@ -73,9 +81,18 @@ public class NotebookContentManager : MonoBehaviour
         image[2] = item2;
 
         BasicInformationVisible(0);
+        BasicInformationVisible(1);
+        BasicInformationVisible(2);
+        BasicInformationVisible(3);
 
+        RevealEvent(4);
+        RevealEvent(3);
     }
 
+    /// <summary>
+    /// Reveals all information about an object, save its Body Text 2
+    /// </summary>
+    /// <param name="pageNumber"></param>
     public void BasicInformationVisible(int pageNumber)
     {
         for (int i = 0; i < ITEMS_PER_PAGE; i++)
@@ -87,9 +104,21 @@ public class NotebookContentManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Reveals all information about a page
+    /// </summary>
+    /// <param name="pageNumber"></param>
     public void AdvancedInformationVisible(int pageNumber)
     {
-        contentVisible[pageNumber, 3] = true;
+        for (int i = 0; i < ITEMS_PER_PAGE; i++)
+        {
+            contentVisible[pageNumber, i] = true;
+        }
+    }
+
+    public void RevealEvent(int eventNumber)
+    {
+        timelineVisible[eventNumber] = true;
     }
 
     #endregion
