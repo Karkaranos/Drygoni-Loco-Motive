4b40a54c7d756cf7fe0554ea24bf427f9a7e7d91
Karkaranos <karkaranosyt@gmail.com>
2023-10-27 13:19:56 -0500

    Scrolling Credits + Bugfixes

--- Loco Motive/Assets/Scripts/UIButtonManager.cs
+++ Loco Motive/Assets/Scripts/UIButtonManager.cs
@@ -13,9 +13,21 @@ using UnityEngine.SceneManagement;
 public class UIButtonManager : MonoBehaviour
 {
     private AudioManager am;
+    private GameObject creditText;
+    private Coroutine stopMe;
+    private GameObject titleCanvas;
+    private GameObject creditCanvas;
+    private Vector2 creditResetPos;
+
     private void Awake()
     {
         am = FindObjectOfType<AudioManager>();
+        creditCanvas = GameObject.Find("CreditCanvas");
+        creditText = GameObject.Find("Credits");
+        creditCanvas.SetActive(false);
+        creditResetPos = creditCanvas.transform.position;
+
+        titleCanvas = GameObject.Find("TitleCanvas");
     }
 
     /// <summary>
@@ -49,6 +61,29 @@ public class UIButtonManager : MonoBehaviour
     {
         SceneManager.LoadScene("TutorialScene");
         am.PlayTutorialMusic();
+    }
+
+    public void Credits()
+    {
+        titleCanvas.SetActive(false);
+        creditCanvas.SetActive(true);
+        stopMe = StartCoroutine(CreditScroll());
+    }
+
+    IEnumerator CreditScroll()
+    {
+       creditText.transform.position = creditResetPos;
+        for (int i=0; i<1350; i += 1)
+        {
+            Vector2 creditPos = creditText.transform.position;
+            creditPos.y += Screen.height/500f;
+            creditText.transform.position = creditPos;
+            yield return new WaitForSeconds(.02f);
+        }
+        yield return new WaitForSeconds(1f);
+        titleCanvas.SetActive(true);
+        creditCanvas.SetActive(false);
+
     }
 
 
